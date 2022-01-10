01b2682b2920a9e77aa30a80f4ca413551ea7253
Jon Fazzaro <jon@fazzaro.com>
2022-01-10 17:16:46 -0500

    wip

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import journal from '../domain/journal';
-import ReactMarkdown from 'react-markdown';
+import React from "react";
+import journal from "../domain/journal";
+import ReactMarkdown from "react-markdown";
 
 function Journal(props) {
-    return <div className="journal">
-    <h1>Journal</h1>
-    {journal.entries(props.cards).map((e, i) => 
-
-    <ReactMarkdown key={i}>e</ReactMarkdown>
-    )}
-    </div>;
+  return (
+    <div className="journal">
+      <h1>Journal</h1>
+      {journal.entries(props.cards).map((e, i) => (
+        <ReactMarkdown key={i}>e</ReactMarkdown>
+      ))}
+    </div>
+  );
 }
 
 export default Journal;
