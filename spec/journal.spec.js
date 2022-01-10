98e3a1892490ac7c4db9729b7c78443eff73f2ca
Jon Fazzaro <jon@fazzaro.com>
2022-01-10 14:33:31 -0500

    wip

--- spec/journal.spec.js
+++ spec/journal.spec.js
@@ -7,35 +7,6 @@ describe("The journal service", () => {
     });
   });
 
-  describe("given one card", () => {
-    it("renders nothing", () => {
-      expect(subject.render([{ name: "Horse shoes", desc: "trill" }])).toEqual(
-        ""
-      );
-    });
-
-    describe('labeled "Close the day"', () => {
-      it("renders its content", () => {
-        expect(
-          subject.render([{ name: "Close the day", desc: "Let's do this" }])
-        ).toEqual("Let's do this");
-      });
-    });
-
-    describe('tagged with "Journal"', () => {
-      it("renders its content", () => {
-        expect(
-          subject.render([
-            {
-              name: "Journal: Thick as a Brick",
-              desc: "The poet and the painter",
-            },
-          ])
-        ).toEqual("The poet and the painter");
-      });
-    });
-  });
-
   describe("given cards", () => {
     it("renders the content from journal cards", () => {
       const cards = [
