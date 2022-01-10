a29f4785354cad2bc72b6c8a1404bac10dc602e4
Jon Fazzaro <jon@fazzaro.com>
2022-01-10 16:36:59 -0500

    wip

--- spec/journal.spec.js
+++ spec/journal.spec.js
@@ -9,14 +9,15 @@ describe("The journal service", () => {
 
   describe("given cards", () => {
     it("renders the content from journal cards", () => {
-      const cards = [
-        { name: "Horse shoes", desc: "trill" },
-        { name: "Close the day", desc: "Really don't mind" },
-        { name: "Journal: Thick as a Brick", desc: "The poet and the painter" },
-      ];
       expect(subject.render(cards)).toEqual(
         `Really don't mind
 The poet and the painter`);
     });
   });
 });
+
+      const cards = [
+        { name: "Horse shoes", desc: "trill" },
+        { name: "Close the day", desc: "Really don't mind" },
+        { name: "Journal: Thick as a Brick", desc: "The poet and the painter" },
+      ];
