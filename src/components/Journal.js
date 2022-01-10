ff70b1364a0d4809233e2e21010c7fd534209b59
Jon Fazzaro <jon@fazzaro.com>
2022-01-10 17:12:20 -0500

    wip

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -3,10 +3,10 @@ import journal from '../domain/journal';
 import ReactMarkdown from 'react-markdown';
 
 function Journal(props) {
-    return <>
+    return <div className="journal">
     <h1>Journal</h1>
     <ReactMarkdown children={journal.render(props.cards)}/>
-    </>;
+    </div>;
 }
 
 export default Journal;
