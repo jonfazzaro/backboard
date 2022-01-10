9c248b613acfaa5bcfacedf0e4334c5f80741703
Jon Fazzaro <jon@fazzaro.com>
2022-01-10 09:24:50 -0500

    wip

--- src/domain/journal.js
+++ src/domain/journal.js
@@ -3,6 +3,7 @@ export default {render};
 function render(cards) {
     if (!cards)
         return "";
+        
     return cards
         .filter(c => c.name === "Close the day")
         .map(c => c.desc)
