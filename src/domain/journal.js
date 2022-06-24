09ae248bee8ab964f7ac582c891bebfb1dadea2c
Jon Fazzaro <jon@fazzaro.com>
2022-06-24 17:37:57 -0400

    wip

--- src/domain/journal.js
+++ src/domain/journal.js
@@ -4,17 +4,14 @@ import moment from "moment";
 export default { entries, stories };
 
 function entries(cards) {
-  const journalCards = _.chain(cards)
-    .filter(isJournal)
-    .orderBy(date)
-    .value();
-    
+  const journalCards = _.chain(cards).filter(isJournal).orderBy(date).value();
+
   return journalCards.map(toJournalEntry);
 }
 
 function stories(cards) {
-  const journal = entries(cards)
-  return journal.flatMap(toStories)
+  const journal = entries(cards);
+  return journal.flatMap(toStories);
 }
 
 function toStories(card) {
@@ -23,38 +20,32 @@ function toStories(card) {
 
 function parseStories(matches) {
   if (matches)
-  return matches.groups.stories
-    .trim()
-    .split('\n')
-    .map(e => e.trim())
-return []
+    return matches.groups.stories
+      .trim()
+      .split("\n")
+      .map(e => e.trim());
+  return [];
 }
 
 function toJournalEntry(card) {
-  return [
-    header(2, title(card)), 
-    newLine, 
-    newLine, 
-    card.desc
-  ].join("");
+  return [header(2, title(card)), newLine, newLine, card.desc].join("");
 }
 
 function title(card) {
-  if (card.name === dailyTitle)
-    return format(createdDate(card));
+  if (card.name === dailyTitle) return format(createdDate(card));
 
   return journalTitle(card);
 }
 
 function createdDate(card) {
-  return new Date(1000*parseInt(card.id.substring(0,8),16));
+  return new Date(1000 * parseInt(card.id.substring(0, 8), 16));
 }
 
 function journalTitle(card) {
   return [
-    card.name.replace(journalTag, "").trim(), 
-    newLine, 
-    header(3, format(card.dateLastActivity))
+    card.name.replace(journalTag, "").trim(),
+    newLine,
+    header(3, format(card.dateLastActivity)),
   ].join("");
 }
 
@@ -63,8 +54,7 @@ function header(level, text) {
 }
 
 function date(card) {
-  if (card.name === dailyTitle)
-    return createdDate(card);
+  if (card.name === dailyTitle) return createdDate(card);
   return new Date(card.dateLastActivity);
 }
 
@@ -73,11 +63,10 @@ function format(date) {
 }
 
 function isJournal(card) {
-  return card.name === dailyTitle 
-      || card.name.startsWith(journalTag);
+  return card.name === dailyTitle || card.name.startsWith(journalTag);
 }
 
 const dailyTitle = "Close the day";
 const journalTag = "Journal:";
 const newLine = "\n";
-const storiesRegex = /### Where's the story\?\s+(?<stories>.+)/s
+const storiesRegex = /### Where's the story\?\s+(?<stories>.+)/s;
