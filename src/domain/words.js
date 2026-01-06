e272fde3686bea01ba976a3122890f639fe8c69f
jonfazzaro <jon@fazzaro.com>
2026-01-06 12:45:23 -0500

    config: add a few more nothing words to the list

--- src/domain/words.js
+++ src/domain/words.js
@@ -1,160 +1,164 @@
-import { flatten, chain, countBy } from 'lodash';
+import {flatten, chain, countBy} from 'lodash';
 
-const Words = { tags, prefixes, words, top, prefix };
+const Words = {tags, prefixes, words, top, prefix};
 export default Words
 
 function tags(cards) {
-    return flatten(
-        cards.map(c => c.labels))
-        .map(l => l.name);
+  return flatten(
+    cards.map(c => c.labels))
+    .map(l => l.name);
 }
 
 function prefixes(cards) {
-    return cards.map(prefix).filter(p => !!p);
+  return cards.map(prefix).filter(p => !!p);
 }
 
 function prefix(card) {
-    if (card.name.indexOf(":") > -1)
-        return card.name.split(":")[0].trim()
+  if (card.name.indexOf(":") > -1)
+    return card.name.split(":")[0].trim()
 
-    return null;
+  return null;
 }
 
 function words(cards) {
-    return flatten(cards.map(wordsFrom)).filter(w => !!w);
+  return flatten(cards.map(wordsFrom)).filter(w => !!w);
 }
 
 function wordsFrom(card) {
-    const alphaNumeric = new RegExp(/^[a-zA-Z0-9_]*$/)
-    return card.name.split(" ")
-        .concat(card.desc.split(" "))
-        .filter(w => common.indexOf(w.trim().toLowerCase()) === -1)
-        .filter(w => alphaNumeric.test(w.trim()));
+  const alphaNumeric = new RegExp(/^[a-zA-Z0-9_]*$/)
+  return card.name.split(" ")
+    .concat(card.desc.split(" "))
+    .filter(w => common.indexOf(w.trim().toLowerCase()) === -1)
+    .filter(w => alphaNumeric.test(w.trim()));
 }
 
 function top(items, count) {
-    return chain(Object.entries(countBy(items, i => i.toLowerCase())))
-        .orderBy(([_, c]) => c, 'desc')
-        .take(count || 10)
-        .value()
-        .map(([k, c]) => {
-            return {
-                text: k,
-                value: c
-            };
-        });
+  return chain(Object.entries(countBy(items, i => i.toLowerCase())))
+    .orderBy(([_, c]) => c, 'desc')
+    .take(count || 10)
+    .value()
+    .map(([k, c]) => {
+      return {
+        text: k,
+        value: c
+      };
+    });
 }
 
 const common = [
-    "are",
-    "please",
-    "i",
-    "is",
-    "your",
-    "you",
-    "year",
-    "would",
-    "work",
-    "with",
-    "will",
-    "who",
-    "which",
-    "when",
-    "what",
-    "well",
-    "we",
-    "way",
-    "want",
-    "use",
-    "us",
-    "up",
-    "two",
-    "to",
-    "time",
-    "this",
-    "think",
-    "they",
-    "these",
-    "there",
-    "then",
-    "them",
-    "their",
-    "the",
-    "that",
-    "than",
-    "take",
-    "some",
-    "so",
-    "she",
-    "see",
-    "say",
-    "people",
-    "over",
-    "out",
-    "our",
-    "other",
-    "or",
-    "only",
-    "one",
-    "on",
-    "of",
-    "now",
-    "not",
-    "no",
-    "new",
-    "my",
-    "most",
-    "me",
-    "make",
-    "look",
-    "like",
-    "know",
-    "just",
-    "its",
-    "it",
-    "into",
-    "in",
-    "if",
-    "I",
-    "how",
-    "his",
-    "him",
-    "her",
-    "he",
-    "have",
-    "good",
-    "go",
-    "give",
-    "get",
-    "from",
-    "for",
-    "first",
-    "even",
-    "do",
-    "day",
-    "could",
-    "come",
-    "can",
-    "by",
-    "but",
-    "because",
-    "be",
-    "back",
-    "at",
-    "as",
-    "any",
-    "and",
-    "an",
-    "also",
-    "all",
-    "after",
-    "about",
-    "a",
-    "was",
-    "were",
-    "had",
-    "am",
-    "close",
-    "has",
-    "been",
+  "are",
+  "please",
+  "i",
+  "is",
+  "your",
+  "you",
+  "year",
+  "would",
+  "work",
+  "with",
+  "will",
+  "who",
+  "which",
+  "when",
+  "what",
+  "well",
+  "we",
+  "way",
+  "want",
+  "use",
+  "us",
+  "up",
+  "two",
+  "to",
+  "time",
+  "this",
+  "think",
+  "they",
+  "these",
+  "there",
+  "then",
+  "them",
+  "their",
+  "the",
+  "that",
+  "than",
+  "take",
+  "some",
+  "so",
+  "she",
+  "see",
+  "say",
+  "people",
+  "over",
+  "out",
+  "our",
+  "other",
+  "or",
+  "only",
+  "one",
+  "on",
+  "of",
+  "now",
+  "not",
+  "no",
+  "new",
+  "my",
+  "most",
+  "me",
+  "make",
+  "look",
+  "like",
+  "know",
+  "just",
+  "its",
+  "it",
+  "into",
+  "in",
+  "if",
+  "I",
+  "how",
+  "his",
+  "him",
+  "her",
+  "he",
+  "have",
+  "good",
+  "go",
+  "give",
+  "get",
+  "from",
+  "for",
+  "first",
+  "even",
+  "do",
+  "day",
+  "could",
+  "come",
+  "can",
+  "by",
+  "but",
+  "because",
+  "be",
+  "back",
+  "at",
+  "as",
+  "any",
+  "and",
+  "an",
+  "also",
+  "all",
+  "after",
+  "about",
+  "a",
+  "was",
+  "were",
+  "had",
+  "am",
+  "close",
+  "has",
+  "been",
+  "got",
+  "here",
+  "via",
+  "find"
 ];
